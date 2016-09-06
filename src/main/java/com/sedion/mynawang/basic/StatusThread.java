9660a89791cbc8d21618dc0883fe24e498bf5f9f
codenewbie <18267853511@163.com>
2016-09-06 09:44:50 +0800

    线程状态包括： NEW RUNNABLE BLOCKED WAITING TIME_WAITING TERMINATED

--- src/main/java/com/sedion/mynawang/basic/StatusThread.java
+++ src/main/java/com/sedion/mynawang/basic/StatusThread.java
@@ -7,7 +7,7 @@ import com.sedion.mynawang.util.SleepUtils;
  * @auther mynawang
  * @create 2016-09-05 22:11
  * NEW          ：初始状态，线程被构建，但是还没有调用start()方法
- * RUNNABLE     ：运行状态，Java线程将操作系统中的就绪和运行两种状态笼统地称作“运行中”
+ * RUNNABLE     ：运行状态，Java线程将操作系统中的就绪(READY)和运行(RUNNING)两种状态笼统地称作“运行中”
  * BLOCKED      ：阻塞状态，表示线程阻塞于锁
  * WAITING      ：等待状态，表示线程进入等待状态，进入该状态表示当前线程需要等待其他线程做出一些特定动作（通知或中断）
  * TIME_WAITING ：超时等待状态，该状态不同于WAITING，它是可以在指定的时间自行返回的
@@ -83,6 +83,38 @@ public class StatusThread {
             }
         }
     }
+
+    /**
+     * 1.NEW（初始状态）------------------Thread.start()-------------------------> 运行(RUNNABLE【RUNNING && READY】)
+     *
+     * 2.RUNNABLE(运行状态)---------------Thread.sleep(long)---------------------> TIME_WAITING(等待超时状态)
+     * 3.RUNNABLE(运行状态)---------------Object.wait(long)----------------------> TIME_WAITING(等待超时状态)
+     * 4.RUNNABLE(运行状态)---------------Thread.join(long)----------------------> TIME_WAITING(等待超时状态)
+     * 5.RUNNABLE(运行状态)---------------LockSupport.parkNanos()----------------> TIME_WAITING(等待超时状态)
+     * 6.RUNNABLE(运行状态)---------------LockSupport.parkUntil()----------------> TIME_WAITING(等待超时状态)
+     *
+     * 7.TIME_WAITING(等待超时状态)-------Object.notify()------------------------> RUNNABLE(运行状态)
+     * 8.TIME_WAITING(等待超时状态)-------Object.notifyAll()---------------------> RUNNABLE(运行状态)
+     * 9.TIME_WAITING(等待超时状态)-------LockSupport.unpark()-------------------> RUNNABLE(运行状态)
+     *
+     * 10.RUNNABLE(运行状态)--------------等待进入synchronized方法---------------> BLOCKED(阻塞状态)
+     * 11.RUNNABLE(运行状态)--------------等待进入synchronized块-----------------> BLOCKED(阻塞状态)
+     *
+     * 12.BLOCKED(阻塞状态)---------------获取到锁-------------------------------> RUNNABLE(运行状态)
+     *
+     * 13.RUNNABLE(运行状态)--------------Object.wait()--------------------------> WAITING(等待状态)
+     * 14.RUNNABLE(运行状态)--------------Object.join()--------------------------> WAITING(等待状态)
+     * 15.RUNNABLE(运行状态)--------------LockSupport.park()---------------------> WAITING(等待状态)
+     *
+     * 16.WAITING(等待状态)---------------Object.notify()------------------------> RUNNABLE(运行状态)
+     * 17.WAITING(等待状态)---------------Object.notifyAll()---------------------> RUNNABLE(运行状态)
+     * 18.WAITING(等待状态)---------------LockSupport.unpark(Thread)-------------> RUNNABLE(运行状态)
+     *
+     * 19.RUNNABLE(运行状态)--------------执行完成-------------------------------> TERMINATED(终止状态)
+     *
+     *
+     */
+
 
 
 }
