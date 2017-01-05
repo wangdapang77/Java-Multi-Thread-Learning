438e078cdbdb6e34d164409823fa1afd5a3180de
mynawang <18267853511@163.com>
2017-01-05 14:16:02 +0800

    _threadlocal调整

--- src/main/java/com/sedion/mynawang/advanced/_ThreadLocal/pra4_inheritablethreadlocal/Tools.java
+++
@@ -1,9 +0,0 @@
-package com.sedion.mynawang.advanced._threadlocal.pra4_inheritablethreadlocal;
-
-/**
- * @auther mynawang
- * @create 2016-12-14 21:28
- */
-public class Tools {
-    public static InheritableThreadLocalExpland inheritableThreadLocalExpland = new InheritableThreadLocalExpland();
-}
